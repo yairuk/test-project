c766df6fcad3585a33d114f10c7d807ff0a82953
Yair Uksusman <yair.uksusman@spectory.com>
2016-12-20 11:15:52 +0200

    node express basic hello world

---
+++ app.js
@@ -0,0 +1,11 @@
+
+var express = require('express')
+var app = express()
+
+app.get('/', function(req,res) {
+    res.send('hello world')
+})
+
+app.listen(5000, function () {
+    console.log('Example app listening on port 5000')
+})
